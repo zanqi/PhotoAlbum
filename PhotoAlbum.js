26ba2297ef2f83e78409c946c5bbc9fbaa2a2d26
Zanqi Liang <Zanqi_Liang@epam.com>
2017-07-14 11:00:25 +0800

    Store the photos

--- PhotoAlbum.js
+++ PhotoAlbum.js
@@ -1,21 +1,33 @@
 'use strict';
 
 function AlbumStore() {
-    
+    this.photos = {};
 }
 
-AlbumStore.prototype.add = function(photo) {
+AlbumStore.prototype.add = function (photo) {
+    this.photos[photo.id] = photo;
+}
 
+AlbumStore.prototype.get = function (id) {
+    return this.photos[id];
 }
 
 function testAdd() {
+    // Arrange
     const albumStore = new AlbumStore();
-    albumStore.add({
+    const input = {
         id: "15",
         desciption: "Family Photo",
         url: 'www.example.com'
-    });
-    
+    };
+
+    // Act
+    albumStore.add(input);
+
+    // Assert
+    console.assert(albumStore.get(input.id).id === input.id);
+    console.assert(albumStore.get(input.id).desciption === input.desciption);
 }
 
 testAdd();
+console.log("Pass");
